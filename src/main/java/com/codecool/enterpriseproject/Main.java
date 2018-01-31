e698c119c7a7cf141d3483f3d707176448a32796
sevendiko <sevendiko@gmail.com>
2018-01-31 15:18:32 +0100

    new test route added

--- src/main/java/com/codecool/enterpriseproject/Main.java
+++ src/main/java/com/codecool/enterpriseproject/Main.java
@@ -1,8 +1,12 @@
 package com.codecool.enterpriseproject;
 
 import com.codecool.enterpriseproject.controller.UserController;
+import com.codecool.enterpriseproject.dbhandler.MessageDbHandler;
+import com.codecool.enterpriseproject.dbhandler.ThreadDbHandler;
 import com.codecool.enterpriseproject.dbhandler.UserDbHandler;
+import com.codecool.enterpriseproject.model.Message;
 import com.codecool.enterpriseproject.model.User;
+import com.codecool.enterpriseproject.model.ChatBox;
 import spark.Request;
 import spark.Response;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
@@ -10,6 +14,8 @@ import spark.template.thymeleaf.ThymeleafTemplateEngine;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
+import java.util.Date;
 
 import static spark.Spark.*;
 
@@ -29,8 +35,6 @@ public class Main {
             return "hello";
         } );
 
-        get ("/test", ((request, response) -> {return "geci";}));
-
         // Always add generic routes to the end
         get( "/register", (Request req, Response res) -> {
             UserDbHandler dbHandler = new UserDbHandler();
@@ -39,6 +43,23 @@ public class Main {
             return new ThymeleafTemplateEngine().render( UserController.renderRegisterPage( req, res ) );
         } );
 
+        get("/testChat", (Request req, Response res) -> {
+            UserDbHandler userDbHandler = new UserDbHandler();
+            ThreadDbHandler threadHandler = new ThreadDbHandler();
+            MessageDbHandler messageHandler = new MessageDbHandler();
+            User sanyika = new User("sanyika", "abarótistartvből", 19, "gyererámpénz");
+            User jolika = new User("jolika", "sanyiszerelme", 17, "adjálpénztsanyi");
+            userDbHandler.addUser(sanyika, em);
+            userDbHandler.addUser(jolika, em);
+            ChatBox chatBox = new ChatBox(sanyika, jolika);
+            Message sanyiÜzenete = new Message(chatBox, new Date(), "ez a message", sanyika);
+            threadHandler.addThread(chatBox, em);
+            messageHandler.addMessage(sanyiÜzenete, em);
+
+            return "siker";
+        });
+
 
     }
+
 }
