9972b885a76faf3cf31de5e1194db8f0a61cd733
sevendiko <sevendiko@gmail.com>
2018-02-15 14:38:12 +0100

    new route for getting new chat partner

--- src/main/java/com/codecool/enterpriseproject/Main.java
+++ src/main/java/com/codecool/enterpriseproject/Main.java
@@ -4,12 +4,10 @@ import com.codecool.enterpriseproject.controller.ChatController;
 import com.codecool.enterpriseproject.controller.UserController;
 import com.codecool.enterpriseproject.dbhandler.ChatBoxDbHandler;
 import com.codecool.enterpriseproject.dbhandler.UserDbHandler;
-import com.codecool.enterpriseproject.model.User;
 import spark.Request;
 import spark.Response;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -54,7 +52,7 @@ public class Main {
             return "";
         });
 
-        post("/doyoulikeme", (request, response) -> new ThymeleafTemplateEngine().render( ChatController.likeOrNot(request, response) ) );
+        post("/doyoulikeme", (request, response) -> new ThymeleafTemplateEngine().render(ChatController.getNewPartner(request, response, chatBoxDbHandler, emf, dbHandler)));
     }
 }
 
