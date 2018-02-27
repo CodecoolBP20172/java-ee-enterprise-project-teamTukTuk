cdcb568eb49c65829631fdff89258ac256f70c2d
Attila Racz <racz.attila89@gmail.com>
2018-02-27 17:18:01 +0100

    bean users added

--- src/main/java/com/codecool/enterpriseproject/service/InitializerBean.java
+++ src/main/java/com/codecool/enterpriseproject/service/InitializerBean.java
@@ -8,5 +8,9 @@ public class InitializerBean {
 
     public InitializerBean(UserService userService) {
         userService.addUser(new User("John", "Doe", "john@email.com", 37, "password", 1, "male", "female"));
+        userService.addUser(new User("Carla", "Jackson", "carla@email.com", 26, "password", 2, "female", "male"));
+        userService.addUser(new User("Clare", "Fraeser", "clare@email.com", 31, "password", 5, "female", "male"));
+        userService.addUser(new User("Mike", "Gregg", "mike@email.com", 22, "password", 8, "male", "female"));
+        userService.addUser(new User("Layla", "Jackson", "layla@email.com", 26, "password", 2, "female", "male"));
     }
 }
